26c85434c1be3013e38e6f058974d290ce6e7ffa
faraz <syed.faraz@wavtec.com>
2025-07-19 12:47:12 +0500

    open_file_functon-created

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,29 @@
+import os
                         #---File-Mangement-Fucntions---#
+"""
+new_file()
+open_file()
+save_file()
+save_as_file()
+close_file()
+exit_application()
+"""
+
 files_dir = './files'
 def new_file(name: str):
     file_path = f'{files_dir}/{name}'
     with open(file_path, 'w') as f:
         f.write(' ')
+
+
+
+def open_file(filename: str):
+    file_path = f'{files_dir}/{filename}'
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            return f.read()
+    else:
+        raise FileNotFoundError(f"File '{filename}' not found in {files_dir}")
+        
+        
+
