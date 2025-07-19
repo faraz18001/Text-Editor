7fb71f3b86960d1eb89b6596672ea04a4cf4995a
faraz <syed.faraz@wavtec.com>
2025-07-19 22:57:14 +0500

    added- nltk support now i need to insert the words inside t he trie export it as pkl file because i don't want to rebuild it again and again when i run the program

--- Backend/trie.py
+++ Backend/trie.py
@@ -1,17 +1,14 @@
+from nltk.corpus import words
+import nltk
+nltk.download('words')
 END_OF_WORD = '*'
 
 def create_trie():
-    """
-    Creates an empty trie, which is just an empty dictionary.
-    This dictionary represents the root node.
-    """
+
     return {}
 
 def insert(trie: dict, word: str):
-    """
-    Inserts a word into the trie.
-    The 'trie' argument is the root dictionary.
-    """
+
     current_node = trie
     for character in word:
         if character not in current_node:
@@ -21,10 +18,7 @@ def insert(trie: dict, word: str):
     current_node[END_OF_WORD] = True
 
 def search(trie: dict, word: str) -> bool:
-    """
-    Searches for a complete word in the trie.
-    Returns True if the word exists, False otherwise.
-    """
+
     current_node = trie
     for character in word:
         if character not in current_node:
@@ -34,9 +28,7 @@ def search(trie: dict, word: str) -> bool:
     return END_OF_WORD in current_node
 
 def starts_with(trie: dict, prefix: str) -> bool:
-    """
-    Checks if there is any word in the trie that starts with the given prefix.
-    """
+
     current_node = trie
     for character in prefix:
         if character not in current_node:
@@ -45,4 +37,8 @@ def starts_with(trie: dict, prefix: str) -> bool:
 
     return True
 
-trie = create_trie()
+#trie = create_trie()
+#
+#
+                    ##--Training and Inserting Words inside the trie----##
+word_list = words.words()#this has around 27500 words
